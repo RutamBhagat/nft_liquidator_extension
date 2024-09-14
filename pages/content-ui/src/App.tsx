18bec93744857437b5f7f8d12c7a4b495f3a9a9e
RutamBhagat <rutambhagat@gmail.com>
2024-09-15 02:55:18 +0530

    feat(eslint): add tailwind class sorting

--- pages/content-ui/src/App.tsx
+++ pages/content-ui/src/App.tsx
@@ -1,7 +1,7 @@
-import { useEffect } from 'react';
 import { Button } from '@extension/ui';
-import { useStorage } from '@extension/shared';
 import { exampleThemeStorage } from '@extension/storage';
+import { useEffect } from 'react';
+import { useStorage } from '@extension/shared';
 
 export default function App() {
   const theme = useStorage(exampleThemeStorage);
@@ -10,14 +10,5 @@ export default function App() {
     console.log('content ui loaded');
   }, []);
 
-  return (
-    <div className="flex items-center justify-between gap-2 bg-blue-100 rounded py-1 px-2">
-      <div className="flex gap-1 text-blue-500">
-        Edit <strong className="text-blue-700">pages/content-ui/src/app.tsx</strong> and save to reload.
-      </div>
-      <Button theme={theme} onClick={exampleThemeStorage.toggle}>
-        Toggle Theme
-      </Button>
-    </div>
-  );
+  return <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4"></div>;
 }
